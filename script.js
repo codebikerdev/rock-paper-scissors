b031a673aa9ae9ae01245bc25ea3b9e0c6fc05db
codebiker <codebiker@yahoo.com>
2022-04-20 16:19:07 -0300

    almost there

--- script.js
+++ script.js
@@ -4,34 +4,27 @@ let result = "";
 game(5);
 
 function game(rounds) {
+  
   for (rounds; rounds > 0; rounds--) {
-    let playerPlay = prompt("Paper, Rock or Scissors?");
-        if (playerPlay.toLowerCase() == "paper")
-        {
-          player = "Paper";
-        }
-        if (playerPlay.toLowerCase() == "rock")
-        {
-          player = "Rock";
-        }
-        if (playerPlay.toLowerCase() == "scissors")
-        {
-        player = "Scissors";
-        }
-        console.log(playRound(player, cpu));
-        console.log(result);
+        let playerPlay = prompt("Paper, Rock or Scissors?");
+          if (playerPlay.toLowerCase() == "paper")
+            {
+              player = "Paper";
+            }
+            if (playerPlay.toLowerCase() == "rock")
+            {
+              player = "Rock";
+            }
+            if (playerPlay.toLowerCase() == "scissors")
+            {
+            player = "Scissors";
+            }
+          console.log(playRound(player, cpu));
+          console.log(result);
       }
-      if (result > 0) {
-        console.log("You Won");
-      }
-      if (result < 0) {
-        console.log("You Lose");
-      } else {
-        console.log("Draw Game");
-      }
-      
-    }
 
+}
+  
 
 function computerPlay(max) {
   let tool = Math.floor(Math.random() * max);
@@ -77,35 +70,11 @@ function playRound(player, cpu) {
 }
 
 
-
-
-
-/*
-function game(rounds) {
-  for (let i = rounds; i > 0; i--) {
-    console.log(playRound(player, cpu));
-  }
+if (result > 0) {
+    console.log("You Won");
 }
-
-game(5);
-*/
-
-
-/*
-console.log(game(5));
-console.log(player);
-console.log("vs")
-console.log(cpu);
-console.log("-----------")
-console.log(computerPlay(3));
-
-
-*/
-
-
-/*
-Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-
-Write a NEW function called game(). Call the playRound function inside of this
-one to play a 5 round game that keeps score and reports a winner or loser at the end. 
-*/
+if (result < 0) {
+  console.log("You Lose");
+} else {
+  console.log("Draw Game");
+}
